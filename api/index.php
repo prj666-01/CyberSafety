6a69f574846db259cdf7745cdf1710dff71a6e67
Ruihui Yan <ryan12@myseneca.ca>
2018-11-02 16:16:28 -0400

    update index.php

--- api/index.php
+++ api/index.php
@@ -2,22 +2,35 @@
 
 
 //Define the constants for the database
-define("DB_HOST", "");
-define("DB_USERNAME", "");
-define("DB_PASSWORD", "");
-define("DB_DATABASE", "");
+define("DB_HOST", "localhost");
+define("DB_USERNAME", "user01");
+define("DB_PASSWORD", "Password123!");
+define("DB_DATABASE", "PRJ666");
 
 $mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+if ($mysqli->connect_error) {
+  //die("Database connection established Failed..");
+}
+else {
+  echo "Connected to " . DB_DATABASE . " database.";
+}
+
+$request = explode("/", ltrim($_SERVER['REQUEST_URI'], '/'));
+array_shift($request);
+
+$requestAction = $request[0];
+$requestTarget = $request[1];
+$RequestTargetId = $request[2];
+
+echo "<strong>Action: </strong>". $requestAction . "<br>";
+echo "<strong>Target: </strong>". $requestTarget . "<br>";
+echo "<strong>Target Id: </strong>". $RequestTargetId . "<br>";
 
 $data = json_decode(file_get_contents("php://input"));
 
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept");
-
-$request = $_SERVER['REQUEST_URI'];
-
-echo $request; 
 
 
 if (!empty($data->title)) {
