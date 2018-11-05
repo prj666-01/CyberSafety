a92ca1a1dd05016b1a59d7a8ba99ea232d6a9f40
Colin <colinchumak91@gmail.com>
2018-11-05 15:37:16 -0500

    Updated CreateCourse page with hiden module add until information submitted

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import HorizontalStepper from 'vue-stepper';
+
+Vue.use(HorizontalStepper);
 Vue.use(BootstrapVue);
 // var session_url = "http://myvmlab.senecacollege.ca:6255/manali/api.php";
 
@@ -13,19 +16,19 @@ var mixin = {
 	},
   methods: {
     foo: function () {
-      console.log('foo')
-    },
-    getAllUsers: function(){
-
-			axios.get("http://myvmlab.senecacollege.ca:6255/api/get/users/1")
-			.then(function(response){
-				if (response.data.error) {
-					vm.errorMessage = response.data.message;
-				}else{
-					vm.users = response.data;
-				}
-			});
-    },
+      // console.log('foo')
+    }
+    // getAllUsers: function(){
+    //
+		// 	axios.get("http://myvmlab.senecacollege.ca:6255/api/get/users/1")
+		// 	.then(function(response){
+		// 		if (response.data.error) {
+		// 			vm.errorMessage = response.data.message;
+		// 		}else{
+		// 			vm.users = response.data;
+		// 		}
+		// 	});
+    // },
     // updateSigninCounter: function(username){
     //   axios.get("http://localhost:8081/api.php?action=updateSigninCounter&username="+ username,{},{
 		// 		// auth:{
