a6443b9339b1b783f7e17e88bad2a01da5f24257
aminmanali <aminnmanali93@gmail.com>
2018-11-22 08:12:39 -0500

    signup and login added

---
+++ basiCerti.php
@@ -0,0 +1,31 @@
+<?php
+ require("includes/header.php");
+ session_start();
+?>
+</head>
+<body>
+<?php
+require("includes/nav.php");
+ ?>
+    <div class="container">
+     
+      <div class="container">
+      <h2>Basic Certificate Courses</h2>
+            <div class="row">
+                <div class="col-sm-4 text-center"> </div>
+
+                <div class="col-sm-4 text-center">
+                <br>
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                    <a href="certification_course/index.php"><img src="includes/images/learning.png" alt="Icon made by Freepik from www.flaticon.com" style="width:150px;height:150px;"></a> 
+                    <br>Basic Certificate Course
+                    </div>     
+                </div> <!--Icons made by Freepik from www.flaticon.com -->
+                </div>
+            </div>
+            </div>
+    </div>
+  <?php
+     require("includes/footer.php");
+    ?>
